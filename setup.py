3ed3e528332d339bcec1188e0174ffdf612e8c6c
Anthony Lozano <anthony.lozano@saifeinc.com>
2017-01-06 17:27:23 -0700

    Added manifest. Modified setup.py a little

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from distutils.core import setup
 setup(
     name='vcapi',
     packages=['vcapi'],
-    version=1.0,
+    version='1.0.0.a1',
     description="Python API wrapper for Veracode's API",
     author="Anthony Lozano",
     author_email='amlozano1@gmail.com',
-    keywords=['veracode', 'api'],
-    requires=['requests', 'click']
-
+    keywords=['veracode', 'api', 'wrapper'],
+    install_requires=['requests', 'click'],
+    url='https://github.com/AnthonyLozano/vcapi',
+    entry_points={
+        'console_scripts': [
+            'vcapi=vcapi.vcapi:main',
+        ],
+    },
 )
